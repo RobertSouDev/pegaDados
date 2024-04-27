d5cb124ccabb4fa52ebf7d489b2f396a129d2f21
RobertSouDev <rogerhallks1@gmail.com>
2024-04-26 23:59:33 -0300

    refatorando o codigo

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 //'https://fakestoreapi.com/products'
+
+// async function pegaDados(){
+//     const resultado =  await fetch('https://fakestoreapi.com/products')
+//     const resultadoConvertido = await resultado.json()
+//     console.log(resultadoConvertido)
+// }
 async function pegaDados(){
-    const resultado =  await fetch('https://fakestoreapi.com/products')
-    const resultadoConvertido = await resultado.json()
-    console.log(resultadoConvertido)
+    const resultado = await fetch('https://fakestoreapi.com/products').then((res)=> res.json())
+
+    console.log(resultado)
 }
 
 pegaDados()
